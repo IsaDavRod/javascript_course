cd9576c3a490b7a5d5ea08abccdecc299f0bfeb6
Isaiah David Rodriguez <rodriisaiah@icloud.com>
2022-03-03 18:59:58 -0600

    Create javascript_course.js

---
+++ javascript_course.js
@@ -0,0 +1,18 @@
+var output = "This is a test!";
+console.log(output);
+
+function calculateArea(length, width) {
+    console.log(length * width);
+}
+
+calculateArea(9123, 2812800);
+
+function triangle_area(base, height) {
+    return (base * height) / 2;
+}
+
+var area = triangle_area(6, 9);
+var areaAgain = triangle_area(4, 20);
+
+console.log(area);
+console.log(areaAgain);
